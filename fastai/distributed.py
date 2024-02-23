178061df5f091cd17fc19448ebe46a8ab5ecf3af
Eugene Mesgar <eugenem@gmail.com>
2024-02-22 19:26:30 -0800

    updates

--- fastai/distributed.py
+++ fastai/distributed.py
@@ -77,9 +77,12 @@ def _round_to_multiple(number,multiple): return int(math.ceil(number/multiple)*m
 # %% ../nbs/20a_distributed.ipynb 18
 class DistributedDL(TfmdDL):
     "A `TfmdDL` which splits a batch into equal size pieces for each worker"
-    def __init__(self,dl,rank=None,world_size=None,device=None):
+    def __init__(self,dl,rank=None,world_size=None,device=None,valid_dl=False):
         if rank is None: rank=rank_distrib()
         if world_size is None: world_size=num_distrib()
+        
+        self.set_valid_dl(valid_dl)
+        print(f"EZM_FASTAI18.0 rank: {rank}, world_size: {world_size}")
         store_attr()
         if type(dl) == torch.utils.data.DataLoader:
             shuffle = True if eq(type(dl.sampler), torch.utils.data.RandomSampler) else False
@@ -92,6 +95,18 @@ class DistributedDL(TfmdDL):
                                   persistent_workers=fake.persistent_workers, 
                                   pin_memory_device=fake.pin_memory_device)
         
+    def set_valid_dl(self, dl):
+        self.is_valid_dl = dl
+
+    def _time_series_split(self, idxs, rank, world_size):
+        "Split `idxs` into equal sized pieces for each worker"
+        new_idxs = []
+        for i, idx in enumerate(idxs):
+            if i % world_size == rank:
+                new_idxs.append(idx)
+
+        return new_idxs
+        
     def _broadcast(self,t,rank):
         "Broadcasts t from rank `rank` to all other ranks. Returns t so t is same for all ranks after call."
         t = LongTensor(t).cuda() # nccl only works with cuda tensors
@@ -99,16 +114,31 @@ class DistributedDL(TfmdDL):
         return t.cpu().tolist()
 
     def _to_detach(self,b,cpu=True,gather=True): return to_detach(b,cpu,gather) # member func so we can override for test
-    def __len__(self): return _round_to_multiple(len(self.dl),self.world_size)//self.world_size
+    def __len__(self): 
+            if self.is_valid_dl:
+                dl_len = len(self.dl)
+                #dl_len += dl_len % self.world_size
+                return dl_len
+            else:   
+                return _round_to_multiple(len(self.dl),self.world_size)//self.world_size
     def get_idxs(self):
         idxs = list(self.dl.get_idxs()) # compute get_idxs in all ranks (we'll only use rank 0 but size must be consistent)
         idxs = self._broadcast(idxs,0)  # broadcast and receive it from rank 0 to all
         self.n = len(idxs)              # we assumed n was dl.n but we really care about number of idxs
         # add extra samples to make it evenly divisible
-        self.n_padded = _round_to_multiple(self.n,self.world_size)
-        idxs += (idxs * (self.n_padded//self.n))[:self.n_padded-self.n] # idx needs to be repeated when n_padded>>n
-        # slice padded idxs so that each rank gets self.n_padded//self.world_size tensors
-        return idxs[self.rank*self.n_padded//self.world_size:(self.rank+1)*self.n_padded//self.world_size]
+
+
+        if self.is_valid_dl:
+            self.n_padded = self.n
+            return idxs
+        else:
+            self.n_padded = _round_to_multiple(self.n,self.world_size)
+            idxs += (idxs * (self.n_padded//self.n))[:self.n_padded-self.n] # idx needs to be repeated when n_padded>>n
+            return self._time_series_split(idxs, self.rank, self.world_size)
+
+        #    return idxs[self.rank*self.n_padded//self.world_size:(self.rank+1)*self.n_padded//self.world_size]
+
+
 
     def before_iter(self):
         self.i = 0
@@ -132,10 +162,10 @@ class DistributedDL(TfmdDL):
             return b
         return apply(_inner,b) if gather and all(hasattr(self,o) for o in ('i','n','n_padded')) else b
 
-# %% ../nbs/20a_distributed.ipynb 29
+# %% ../nbs/20a_distributed.ipynb 30
 _hidden_params = ["mixed_precision", "fp16", "log_with", "logging_dir", "step_scheduler_with_optimizer"]
 
-# %% ../nbs/20a_distributed.ipynb 30
+# %% ../nbs/20a_distributed.ipynb 31
 class DistributedTrainer(Callback):
     "Wrap `model` in `DistributedDataParallel` and `dls` in `DistributedDL`"
     order = 11
@@ -154,14 +184,21 @@ class DistributedTrainer(Callback):
         self.learn.dls.loaders = [self._wrap_dl(dl) for dl in self.dls]
         if rank_distrib(): self.learn.logger=noop
 
-    def _wrap_dl(self, dl): return dl if isinstance(dl,DistributedDL) else DistributedDL(dl, device=self.learn.model.device)
+    def _wrap_dl(self, dl, set_valid=False): 
+        if isinstance(dl, DistributedDL): 
+            dl.set_valid_dl(set_valid)
+            return dl
+        else:
+            return DistributedDL(dl, device=self.learn.model.device, valid_dl=set_valid)
+        
+        #return dl if isinstance(dl,DistributedDL) else DistributedDL(dl, device=self.learn.model.device)
     def _backward(self): self.accelerator.backward(self.learn.loss_grad)
     
-    def before_train(self):    self.learn.dl = self._wrap_dl(self.learn.dl)
-    def before_validate(self): self.learn.dl = self._wrap_dl(self.learn.dl)
+    def before_train(self):    self.learn.dl = self._wrap_dl(self.learn.dl, set_valid=False)
+    def before_validate(self): self.learn.dl = self._wrap_dl(self.learn.dl, set_valid=True)
     def after_fit(self): self.learn.model,self.learn.dls.loaders = self.learn.model.module,self.old_dls
 
-# %% ../nbs/20a_distributed.ipynb 31
+# %% ../nbs/20a_distributed.ipynb 32
 @patch
 @delegates(Accelerator, but=_hidden_params)
 def to_distributed(self: Learner,
@@ -173,7 +210,7 @@ def to_distributed(self: Learner,
     if rank_distrib(): self.remove_cb(ProgressCallback)
     return self
 
-# %% ../nbs/20a_distributed.ipynb 32
+# %% ../nbs/20a_distributed.ipynb 33
 @patch
 def detach_distributed(self: Learner):
     "Remove `DistributedTrainer` from a learner"
@@ -182,7 +219,7 @@ def detach_distributed(self: Learner):
     if rank_distrib() and not hasattr(self, 'progress'): self.add_cb(ProgressCallback())
     return self
 
-# %% ../nbs/20a_distributed.ipynb 34
+# %% ../nbs/20a_distributed.ipynb 35
 @patch
 @contextmanager
 @delegates(Accelerator, but=_hidden_params)
@@ -211,7 +248,7 @@ def distrib_ctx(self: Learner,
         self.detach_distributed()
         if cleanup_dpg: teardown_distrib()
 
-# %% ../nbs/20a_distributed.ipynb 36
+# %% ../nbs/20a_distributed.ipynb 37
 def rank0_first(func, *args, **kwargs):
     "Execute `func` in the Rank-0 process first, then in other ranks in parallel."
     if args or kwargs: func = partial(func, *args, **kwargs)
